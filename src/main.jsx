0bdddb95b7dee21a4c7dc9da7d67608b11006b8e
Wpstorm <wpstormdev@gmail.com>
2023-06-10 14:22:35 +0330

    feat: Completed part 2 of the course

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {Provider} from "react-redux";
-import {createStore, applyMiddleware, compose} from "redux";
-import thunk from "redux-thunk";
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 
-import reducers from './reducers'
-import App from './App.jsx'
-import './index.css'
+import {reducers} from './reducers';
+import App from './App';
+import './index.css';
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)))
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const container = document.getElementById('root');
+const root = createRoot(container)
+
+root.render(
     <Provider store={store}>
-        <App />
-    </Provider>,
+        <App/>
+    </Provider>
 )
